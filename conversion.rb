c8a0e4cdc48bdf317fa936fa828bc7e8dae6514b
Raymond Hangwei Ou <hangway@Raymonds-MacBook-Pro.local>
2016-06-14 07:21:33 -0700

    conversion
    L*W

---
+++ conversion.rb
@@ -0,0 +1,13 @@
+conversion = 10.7639
+
+puts 'Enter the length of the room in meters: '
+length = gets.to_f
+
+puts 'Enter the width of the room in meters: '
+width = gets.to_f
+
+square_meters = (length * width).round(2)
+square_feet = (square_meters * conversion).round(2)
+
+puts "The area of the room is #{square_meters} " + \
+     "square meters (#{square_feet} square feet)."
